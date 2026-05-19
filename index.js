07e4393d9176e3515df12cc57e7532448a034dea
Ashik <ashikurashik20999@gmail.com>
2026-05-19 08:25:08 +0600

    add start script

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGO_URL;
 const admin = require("firebase-admin");
-const serviceAccount = require("./zap-shift-01-firebase-adminsdk.json");
+// const serviceAccount = require("./zap-shift-01-firebase-adminsdk.json"); 
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
 
 // middlewear
 app.use(express.json());
@@ -611,8 +616,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
